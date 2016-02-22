0633dbdd489a4edfcefdbee042194160ec2e8b79
AJP <allenatmc@gmail.com>
2016-02-22 09:46:45 -0600

    add body parser and login boilerplate

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 "use strict";
 
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'jade');
 
 app.get('/', (req, res) => {
+  res.redirect('login');
+});
+
+app.get('/home', (req, res) => {
   res.render('index');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  res.redirect('/home');
 });
 
 app.listen(PORT, (err) => {
